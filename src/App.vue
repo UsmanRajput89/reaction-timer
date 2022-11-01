4cba5184c9d8447925ff3e51f552c406f60c4977
UsmanRajput89 <usmanrana18989@gmail.com>
2022-11-02 01:16:42 +0500

    First Repo

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,45 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>My Reaction Timer</h1>
+    <button @click="start" :disabled="isPlaying">Play</button>
+
+    <Block v-if="isPlaying" :delay="delay" />
+         
+    
+    <!-- </Block> -->
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Block from './components/Block.vue';
+
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: 'App',
+    components: {Block},
+    data(){
+        return {
+            isPlaying:false,
+            delay:null,
+
+        }
+    },
+    methods: {
+        start(){
+            this.isPlaying = true;
+            this.delay = 2000 + Math.random() * 5000;
+            // console.log(this.delay); 
+        }
+    }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #444;
+    margin-top: 40px;
 }
 </style>
